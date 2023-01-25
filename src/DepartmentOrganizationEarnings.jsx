c57bc24f0b2f48c0050d14acff57fd9026230643
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-01-25 14:46:24 -0500

    Gets data from form to set department and pass the department to back end through get request to get back organizations of that department

--- src/DepartmentOrganizationEarnings.jsx
+++ src/DepartmentOrganizationEarnings.jsx
@@ -3,28 +3,53 @@ import { useState } from "react";
 
 export function DepartmentOrganizationEarnigns() {
   const [departmentOrganizationEarnings, setDepartmentOrganizationEarnings] = useState([]);
+  const [department, setDepartment] = useState("");
 
   const handleOrganizationEarnings = (department) => {
-    axios.get("http://localhost:3000/department_organizations_earnings.json", department).then((response) => {
-      console.log(response.data);
-    });
+    axios
+      .get("http://localhost:3000/department_organizations_earnings.json", { params: { department } })
+      .then((response) => {
+        console.log(response.data);
+        setDepartmentOrganizationEarnings(response.data);
+      });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const params = new FormData(event.target);
-    event.target.reset;
+    setDepartment(params.values().next().value);
+    handleOrganizationEarnings(department);
+    event.target.reset();
   };
 
   return (
     <div>
-      <p>asd</p>
+      {department.length > 0 ? (
+        <>
+          <h2>Organization Earnings for department: {department}</h2>
+        </>
+      ) : (
+        <>
+          <h2>Organization Earnings for department</h2>
+        </>
+      )}
+
       <form onSubmit={handleSubmit}>
         <label>Enter Department</label>
         <input name="department" type="text" />
 
         <button type="Submit">Submit</button>
       </form>
+
+      {departmentOrganizationEarnings.length > 0 ? (
+        <>
+          <h2>Gottem</h2>
+        </>
+      ) : (
+        <>
+          <h2>Loading...</h2>
+        </>
+      )}
     </div>
   );
 }
