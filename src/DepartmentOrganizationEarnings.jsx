8abb526795d121bb6e2a1f77792c7663e5e0bb3f
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-01-25 13:47:52 -0500

    Adds request to backend for department organization earnings as well as form for getting the department

--- src/DepartmentOrganizationEarnings.jsx
+++ src/DepartmentOrganizationEarnings.jsx
@@ -1,7 +1,30 @@
-export function departmentOrganizationEarnigns() {
+import axios from "axios";
+import { useState } from "react";
+
+export function DepartmentOrganizationEarnigns() {
+  const [departmentOrganizationEarnings, setDepartmentOrganizationEarnings] = useState([]);
+
+  const handleOrganizationEarnings = (department) => {
+    axios.get("http://localhost:3000/department_organizations_earnings.json", department).then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    event.target.reset;
+  };
+
   return (
     <div>
       <p>asd</p>
+      <form onSubmit={handleSubmit}>
+        <label>Enter Department</label>
+        <input name="department" type="text" />
+
+        <button type="Submit">Submit</button>
+      </form>
     </div>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import { RevenueIndex } from "./RevenueIndex";
+import { DepartmentOrganizationEarnigns } from "./DepartmentOrganizationEarnings";
 
 export function Home() {
   return (
     <div>
       <h1>Welcome to React!</h1>
       <RevenueIndex />
+      <DepartmentOrganizationEarnigns />
     </div>
   );
 }
